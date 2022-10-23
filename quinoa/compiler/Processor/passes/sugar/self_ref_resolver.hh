fbf93add8d84080aea30cdde61748692ffc11a17
caseyallen <caseyallen1024@gmail.com>
2022-10-23 20:08:30 +0100

    Re-implemented self-ref resolver

--- quinoa/compiler/Processor/passes/sugar/self_ref_resolver.hh
+++ quinoa/compiler/Processor/passes/sugar/self_ref_resolver.hh
@@ -2,24 +2,28 @@
 #include "../include.h"
 using namespace std;
 
-// void resolve_self_refs(SourceBlock *content, CompoundIdentifier* space) {
-//   auto flat = content->flatten();
+void resolve_self_refs(SourceBlock *content, ModuleRef *mod)
+{
+  auto flat = content->flatten();
 
-//   for (auto m : flat) {
-//     if (instanceof<MethodCall>(m)) {
-//       auto call = (MethodCall *)m;
-//       auto ctx = call->ctx;
-//       if(call->builtin())continue;
-//       call->name->flatify();
-//       if (call->name->size() == 1) {
-//         pushf(call->name->parts, (Identifier *)space);
-//         call->name->flatify();
-//       }
-//     }
-//   }
-// }
-void resolve_self_refs(CompilationUnit &unit) {
-    // for (auto fn : unit.getAllMethods()) {
-      // resolve_self_refs(fn, fn->fullname()->all_but_last());
-    // }
+  for (auto m : flat)
+  {
+    if (instanceof <MethodCall>(m))
+    {
+      auto call = (MethodCall *)m;
+      auto ctx = call->ctx;
+      if (call->builtin())
+        continue;
+      if(!call->name->mod){
+        call->name->mod = mod;
+      }
+    }
+  }
+}
+void resolve_self_refs(CompilationUnit &unit)
+{
+  for (auto fn : unit.getAllMethods())
+  {
+    resolve_self_refs(fn, fn->sig->name->mod );
+  }
 }
